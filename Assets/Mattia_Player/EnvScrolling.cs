7b2a54939847c1e3b748e57e6d5e3134a7517d40
MattiaPezzanoAiv <mattiapezzano7@gmail.com>
2018-01-27 02:40:28 +0100

    add static environment scrolling

---
+++ Assets/Mattia_Player/EnvScrolling.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvScrolling : MonoBehaviour {
+
+    private Material material;
+
+    [SerializeField]
+    private Vector2 scrolling;
+
+    void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        material.mainTextureOffset += scrolling * Time.deltaTime;
+	}
+}
