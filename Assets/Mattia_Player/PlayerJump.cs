c09db96b1e24cf21148b9eec71be816d22577a9c
MattiaPezzanoAiv <mattiapezzano7@gmail.com>
2018-01-27 02:24:23 +0100

    add movements and jump behaviour

---
+++ Assets/Mattia_Player/PlayerJump.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController), typeof(PlayerMovement))]
+public class PlayerJump : MonoBehaviour
+{
+
+    [SerializeField]
+    private Vector3 gravity;
+    private Vector3 gravityAccumulator;
+
+    [SerializeField]
+    private Vector3 jumpForce;
+
+    private CharacterController controller;
+    private PlayerMovement movement;
+    private bool gravityEnabled;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        movement = GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        if(gravityEnabled)
+        {
+            gravityAccumulator += gravity * Time.deltaTime;
+            CollisionFlags flags = controller.Move(gravityAccumulator * Time.deltaTime);
+
+            if((flags & (CollisionFlags.Below | CollisionFlags.CollidedBelow)) != 0)
+            {
+                gravityEnabled = false;
+                gravityAccumulator = Vector3.zero;
+            }
+        }
+
+        if(!gravityEnabled && GetJump())
+        {
+            gravityEnabled = true;
+            gravityAccumulator = jumpForce;
+            Debug.Log("jump");
+        }
+    }
+
+    private bool GetJump()
+    {
+        int p = movement.GetPlayer();
+        if(p == 1)
+        {
+            return (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button1));    //recalibration
+        }
+        else if(p == 2)
+        {
+            return (Input.GetKeyDown(KeyCode.RightControl) || Input.GetKeyDown(KeyCode.Joystick2Button1));    //recalibration
+        }
+        return false;
+    }
+}
---
+++ Assets/Mattia_Player/PlayerMovement.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    [Range(1, 2)]
+    private int player;                                 //number of player
+
+
+    [SerializeField]
+    private bool smoothAcceleration;
+    [SerializeField]
+    [Tooltip("Is ignored if smoothAcceleration param is toggled off")]
+    private float smoothDelta;
+    [SerializeField]
+    private float speed;
+
+    private float hor;
+    private float ver;
+    private CharacterController controller;
+
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (var item in Input.GetJoystickNames())
+        {
+            Debug.Log(item);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float _hor = Clamp(Input.GetAxis(GetHorizontal()));
+        float _ver = Clamp(Input.GetAxis(GetVertical()));
+        if (smoothAcceleration)
+        {
+            hor = Mathf.Lerp(hor, _hor, Time.deltaTime * smoothDelta);
+            ver = Mathf.Lerp(ver,_ver, Time.deltaTime * smoothDelta);
+        }
+        else
+        {
+            hor = _hor;
+            ver = _ver;
+        }
+
+        Vector3 dir = Camera.main.transform.forward * ver + hor * Camera.main.transform.right;
+        dir.y = 0;
+        controller.Move(dir * speed * Time.deltaTime);
+        Debug.Log("moved");
+    }
+
+    private string GetVertical()
+    {
+        if (player == 1) return "Vertical1";
+        return "Vertical2";
+    }
+    private string GetHorizontal()
+    {
+        if (player == 1) return "Horizontal1";
+        return "Horizontal2";
+    }
+    private float Clamp(float value)
+    {
+        if (value >= -0.2f && value <= 0.2f)
+            return 0;
+        return value;
+    }
+
+
+
+
+    public int GetPlayer()
+    {
+        return player;
+    }
+}
