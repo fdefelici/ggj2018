eed362f8541ed07ea73fb0cff58cddf23ec7be42
unknown <emanuele.freschi@gmail.com>
2018-01-27 13:17:27 +0100

    scriptable audio object and test ghost

---
+++ Assets/Scenes/SoundsList.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu ( fileName = "new Audio", menuName = "Sounds")]
+public class SoundsList : ScriptableObject
+{
+    public AudioClip Jump;
+    public AudioClip TakeDisturbation;
+    public AudioClip Hurt;
+    public AudioClip ChangePosition;
+    
+}
