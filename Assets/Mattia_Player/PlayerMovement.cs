8236bf294d15c14d03e35712f478d13c8a5db5ac
MattiaPezzanoAiv <mattiapezzano7@gmail.com>
2018-01-27 03:58:04 +0100

    updated movements with screen movements limit

--- Assets/Mattia_Player/PlayerMovement.cs
+++ Assets/Mattia_Player/PlayerMovement.cs
@@ -47,7 +47,7 @@ public class PlayerMovement : MonoBehaviour
         if (smoothAcceleration)
         {
             hor = Mathf.Lerp(hor, _hor, Time.deltaTime * smoothDelta);
-            ver = Mathf.Lerp(ver,_ver, Time.deltaTime * smoothDelta);
+            ver = Mathf.Lerp(ver, _ver, Time.deltaTime * smoothDelta);
         }
         else
         {
@@ -55,10 +55,12 @@ public class PlayerMovement : MonoBehaviour
             ver = _ver;
         }
 
-        Vector3 dir = Camera.main.transform.forward * ver + hor * Camera.main.transform.right;
-        dir.y = 0;
-        controller.Move(dir * speed * Time.deltaTime);
-        Debug.Log("moved");
+        Vector3 dir = GetMovementDirection();
+
+        Vector3 newPos = transform.position + (dir * speed * Time.deltaTime);
+
+        if (IsInScreen(newPos))
+            controller.Move(dir * speed * Time.deltaTime);
     }
 
     private string GetVertical()
@@ -76,6 +78,40 @@ public class PlayerMovement : MonoBehaviour
         if (value >= -0.2f && value <= 0.2f)
             return 0;
         return value;
+    }
+    private bool IsInScreen(Vector3 pos)
+    {
+        if (player == 1)
+        {
+            Vector3 sPos = GameObject.FindGameObjectWithTag("Camera1").GetComponent<Camera>().WorldToScreenPoint(pos);
+            if (!(sPos.x <= 0 + 25 || sPos.x > Screen.width / 2 - 25) && !(sPos.y <= 0 || sPos.y > Screen.height / 2 + 25))                           //not out of window
+                return true;
+            return false;
+        }
+        else if (player == 2)
+        {
+            Vector3 sPos = GameObject.FindGameObjectWithTag("Camera2").GetComponent<Camera>().WorldToScreenPoint(pos);
+            if (!(sPos.x <= Screen.width / 2 + 25 || sPos.x > Screen.width - 25) && !(sPos.y <= 0 || sPos.y > Screen.height / 2 + 25))                           //not out of window
+                return true;
+            return false;
+        }
+        return false;
+    }
+    private Vector3 GetMovementDirection()
+    {
+        Camera c = null;
+        if (player == 1)
+        {
+            c = GameObject.FindGameObjectWithTag("Camera1").GetComponent<Camera>();
+        }
+        else if (player == 2)
+        {
+            c = GameObject.FindGameObjectWithTag("Camera2").GetComponent<Camera>();
+        }
+        //Vector3 dir = hor * c.transform.right + ver * c.transform.forward;
+        Vector3 dir = Vector3.right * hor + Vector3.forward * ver;
+        dir.y = 0;
+        return dir;
     }
 
 
