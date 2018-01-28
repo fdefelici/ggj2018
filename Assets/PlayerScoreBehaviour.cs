0635069ceb00ad6f6477efc7466f7e40199881b2
Federico De Felici <fdf8210@gmail.com>
2018-01-28 15:25:32 +0100

    no message

---
+++ Assets/PlayerScoreBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScoreBehaviour : MonoBehaviour {
+
+    public Text _pointText;
+
+    void OnTriggerEnter(Collider aCollider) {
+        PerkBeahviour perk = aCollider.GetComponent<PerkBeahviour>();
+        if (perk != null) return; //Se è un perk non influisce sui punti
+
+        int currentPoints = int.Parse(_pointText.text);
+        currentPoints -= 100;
+        _pointText.text = currentPoints+"";
+    }
+
+}
