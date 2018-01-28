467a5769ae96a82c31fde8748ee817d1dfdb6020
unknown <emanuele.freschi@gmail.com>
2018-01-28 13:07:32 +0100

    ui set and clock

---
+++ Assets/CanvasBehaviours.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasBehaviours : MonoBehaviour
+{
+    public Sprite[] Numbers;
+    public GameObject Minutes;
+    public GameObject FirstSecond;
+    public GameObject SecondSecond;
+
+    public int Seconds;
+    public int AssignedMinutes;
+    private float delta;
+    private int count;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		delta += Time.deltaTime;
+        if(delta >= 1 )
+        {
+            Seconds -= 1;
+            delta = 0;
+            count -= 1;
+            if (count<0)
+            {
+                count = 9;
+            }
+        }
+
+        Clock();
+	}
+
+    void Clock()
+    {
+        if (Seconds <= 0)
+        {
+            Minutes.GetComponent<Image>().sprite = Numbers[0];
+            FirstSecond.GetComponent<Image>().sprite = Numbers[0];
+            SecondSecond.GetComponent<Image>().sprite = Numbers[0];
+        }
+        else
+        {
+
+
+            int resoult = Seconds / 60;
+            Minutes.GetComponent<Image>().sprite = Numbers[resoult];
+            Debug.Log(resoult);
+
+            int resoult2 = (Seconds % 60) / 10; //-*10
+            FirstSecond.GetComponent<Image>().sprite = Numbers[resoult2];
+
+            SecondSecond.GetComponent<Image>().sprite = Numbers[count];
+        }
+
+        
+    }
+}
